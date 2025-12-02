b09c45b784cf6fb5e137acc2215ad84873baca10
Russoboy <adigundaniel14@gmail.com>
2025-12-02 15:21:14 +0100

    Navbar.jsx

--- Frontend/nft-frontend/src/App.jsx
+++ Frontend/nft-frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MintNFT from "./components/MintNFT";
 import "./styles.css";
-
+import Navbar from "./components/Navbar"
 const App = () => {
   return (
     <div className="app">
-      <h1>My NFT Minting DApp</h1>
-      <MintNFT />
+      <h1 className="NFT-brand-name">OpulentMint</h1>
+      <Navbar />
+      {/* <MintNFT /> */}
     </div>
   );
 };
---
+++ Frontend/nft-frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const  Navbar = () =>{
+     
+
+
+
+
+    return(
+    <nav className="Navbar">
+      <ul>
+        <li>Home</li>
+        <li>Marketplace</li>
+        <li>Mint NFT</li>
+        <li>Collections</li>
+        <li>About/Docs</li>
+      </ul>
+    </nav>
+    )
+}
+
+
+export default  Navbar;
